04454609f9e13fa86f3e38344733f97a0c373396
tpereztorres <tonipereztorres@gmail.com>
2018-03-05 20:35:12 +0100

    Add files via upload

--- common/config.py
+++ common/config.py
@@ -24,5 +24,18 @@ elif DATABASE == "changedetection":
     abs_dir_gt = os.path.join(dir_databases, DATABASE, "highway", dir_gt)
     abs_dir_input = os.path.join(dir_databases, DATABASE, "highway", dir_input)
 
+elif DATABASE == "fall":
+    #abs_dir_result = os.path.join(dir_databases, DATABASE, dir_results)
+    abs_dir_gt = os.path.join(dir_databases, DATABASE, dir_gt)
+    abs_dir_input = os.path.join(dir_databases, DATABASE, dir_input)
+
+elif DATABASE == "traffic":
+    #abs_dir_result = os.path.join(dir_databases, DATABASE, dir_results)
+    abs_dir_gt = os.path.join(dir_databases, DATABASE,  dir_gt)
+    abs_dir_input = os.path.join(dir_databases, DATABASE, dir_input)
+
 else:
     raise ValueError(DATABASE + " DB does not exist!")
+
+
+
