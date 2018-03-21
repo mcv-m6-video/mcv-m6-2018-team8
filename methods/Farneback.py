c251e074293b4e945c6bab39e1b3ec5eeea7226f
Arnau Casadevall <arnau.casadevall@uab.cat>
2018-03-21 10:59:04 +0100

    minor bugs

--- methods/Farneback.py
+++ methods/Farneback.py
@@ -1,10 +1,16 @@
 import cv2
 
-def Farneback(im1, im2):
+def Farneback(im1, im2, win=7):
 
-    prvs = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
-    next = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
+    assert (im1.shape == im2.shape)
 
-    flow = cv2.calcOpticalFlowFarneback(prvs, next, 0.5, 3, 15, 3, 5, 1.2, 0)
+    if im1.ndim == 3:
+        im1 = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
+
+    if im2.ndim == 3:
+        im2 = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
+
+    flow = cv2.calcOpticalFlowFarneback(im1, im2, None, 0.5, 3, win, 3, 5, 1.2, 0)
+    # flow = cv2.calcOpticalFlowFarneback(prvs, next, 0.5, 5, 15, 9, 7, 1.5, cv2.OPTFLOW_FARNEBACK_GAUSSIAN)
 
     return flow
